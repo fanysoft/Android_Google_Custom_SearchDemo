d892653a619db11e2cc720f0db8f1d3493d982ad
Josef Vancura <josef.vancura@email.cz>
2020-01-15 11:21:13 +0100

    update 2020 - test new foo change

---
+++ README.md
@@ -0,0 +1,9 @@
+# Android_Google_Custom_SearchDemo
+Android demo for Google Search
+
+Tutorial
+http://www.vancura.cz/?p=578
+
+Video
+https://www.youtube.com/watch?v=VpJlPhqZuoU
+
---
+++ app/src/main/java/cz/vancura/searchapp/MainActivity.java
@@ -0,0 +1,197 @@
+package cz.vancura.searchapp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity {
+
+    EditText mEditText;
+    Button mButton;
+    TextView mTextView;
+    ProgressBar mProgressBar;
+    Context context;
+
+    private static final String TAG = "searchApp";
+    static String result = null;
+    Integer responseCode = null;
+    String responseMessage = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "**** APP START ****");
+        context = getApplicationContext();
+
+        // GUI init
+        mEditText = findViewById(R.id.edittext);
+        mButton = findViewById(R.id.button);
+        mTextView = findViewById(R.id.textView1);
+        mProgressBar = findViewById(R.id.pb_loading_indicator);
+
+        // button onClick
+        mButton.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+
+                final String searchString = mEditText.getText().toString();
+                String txt = "Searching for : " + searchString;
+                Log.d(TAG, txt);
+                mTextView.setText(txt);
+
+                // hide keyboard
+                InputMethodManager inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+
+                // remove spaces
+                String searchStringNoSpaces = searchString.replace(" ", "+");
+
+                // Your Google API key
+                // TODO replace with your value
+                //String key="AIzaSyAY4lFujVxjP.........";
+                String key="AIzaSyBZdsVKDN-i8Ayh3lFHvgOVrV0AH-5LOw0";
+
+                // Your Google Search Engine ID
+                // TODO replace with your value
+                //String cx = "0084928477647518.........";
+                String cx = "008492847764751849638:0dqsv4vjwh4";
+
+                String urlString = "https://www.googleapis.com/customsearch/v1?q=" + searchStringNoSpaces + "&key=" + key + "&cx=" + cx + "&alt=json";
+                URL url = null;
+                try {
+                    url = new URL(urlString);
+                } catch (MalformedURLException e) {
+                    Log.e(TAG, "ERROR converting String to URL " + e.toString());
+                }
+                Log.d(TAG, "Url = "+  urlString);
+
+
+                // start AsyncTask
+                GoogleSearchAsyncTask searchTask = new GoogleSearchAsyncTask();
+                searchTask.execute(url);
+
+            }
+        });
+
+    }
+
+
+    private class GoogleSearchAsyncTask extends AsyncTask<URL, Integer, String>{
+
+        protected void onPreExecute(){
+
+            Log.d(TAG, "AsyncTask - onPreExecute");
+            // show mProgressBar
+            mProgressBar.setVisibility(View.VISIBLE);
+
+        }
+
+
+        @Override
+        protected String doInBackground(URL... urls) {
+
+            URL url = urls[0];
+            Log.d(TAG, "AsyncTask - doInBackground, url=" + url);
+
+            // Http connection
+            HttpURLConnection conn = null;
+            try {
+                conn = (HttpURLConnection) url.openConnection();
+            } catch (IOException e) {
+                Log.e(TAG, "Http connection ERROR " + e.toString());
+            }
+
+
+            try {
+                responseCode = conn.getResponseCode();
+                responseMessage = conn.getResponseMessage();
+            } catch (IOException e) {
+                Log.e(TAG, "Http getting response code ERROR " + e.toString());
+
+            }
+
+            Log.d(TAG, "Http response code =" + responseCode + " message=" + responseMessage);
+
+            try {
+
+                if(responseCode != null && responseCode == 200) {
+
+                    // response OK
+
+                    BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    StringBuilder sb = new StringBuilder();
+                    String line;
+
+                    while ((line = rd.readLine()) != null) {
+                        sb.append(line + "\n");
+                    }
+                    rd.close();
+
+                    conn.disconnect();
+
+                    result = sb.toString();
+
+                    Log.d(TAG, "result=" + result);
+
+                    return result;
+
+                }else{
+
+                    // response problem
+
+                    String errorMsg = "Http ERROR response " + responseMessage + "\n" + "Are you online ? " + "\n" + "Make sure to replace in code your own Google API key and Search Engine ID";
+                    Log.e(TAG, errorMsg);
+                    result = errorMsg;
+                    return  result;
+
+                }
+            } catch (IOException e) {
+                Log.e(TAG, "Http Response ERROR " + e.toString());
+            }
+
+            return null;
+        }
+
+        protected void onProgressUpdate(Integer... progress) {
+
+            Log.d(TAG, "AsyncTask - onProgressUpdate, progress=" + progress);
+
+        }
+
+        protected void onPostExecute(String result) {
+
+            Log.d(TAG, "AsyncTask - onPostExecute, result=" + result);
+
+            // hide mProgressBar
+            mProgressBar.setVisibility(View.GONE);
+
+            // make TextView scrollable
+            mTextView.setMovementMethod(new ScrollingMovementMethod());
+            // show result
+            mTextView.setText(result);
+
+        }
+
+
+    }
+
+}
